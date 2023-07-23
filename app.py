96f38c175757aacb45c2e2cc46ff15c6af6137e7
kareemadesola <kareemadesola1999@gmail.com>
2023-07-23 12:19:36 +0100

    using only one route

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from flask import Flask, request, render_template
 
 from data import SYMPTOMS_OPTIONS
@@ -8,20 +6,22 @@ from ml_model import generate_recommendations
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', symptoms=SYMPTOMS_OPTIONS)  # Pass the symptoms list to the template
+    if request.method == 'POST':
+        # Retrieve selected symptoms from the form submission
+        selected_symptoms = request.form.getlist('symptoms')
 
+        if not selected_symptoms:
+            # If no symptoms are selected, return an empty list as recommendations
+            return render_template('index.html', symptoms=SYMPTOMS_OPTIONS, recommendations=[])
 
-@app.route('/recommendations', methods=['POST'])
-def get_recommendations():
-    # Retrieve selected symptoms from the form submission
-    selected_symptoms: List[str] = request.form.getlist('symptoms')
+        # Generate exercise recommendations based on selected symptoms
+        recommended_exercises = generate_recommendations(selected_symptoms)
 
-    # Generate exercise recommendations based on selected symptoms
-    recommended_exercises = generate_recommendations(selected_symptoms)
-
-    return render_template('index.html', recommendations=recommended_exercises)
+        return render_template('index.html', symptoms=SYMPTOMS_OPTIONS, recommendations=recommended_exercises)
+    else:
+        return render_template('index.html', symptoms=SYMPTOMS_OPTIONS)
 
 
 if __name__ == '__main__':
