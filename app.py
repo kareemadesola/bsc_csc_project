50f62ee6c9b0d6a546623a68805dbf1f1ff5f4fd
kareemadesola <kareemadesola1999@gmail.com>
2023-09-28 01:53:51 +0100

    implement the login functionality

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, session
 
 from data import SYMPTOMS_OPTIONS
 from ml_model import generate_recommendations
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a strong, random secret key
+
+# Define a variable to track the logged-in state
+logged_in = False
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global logged_in
+    if request.method == 'POST':
+        # Check username and password (you can replace this with your authentication logic)
+        username = request.form['username']
+        password = request.form['password']
+
+        # Replace this with your actual authentication logic
+        if username == 'adesola' and password == 'kareem':
+            # Successful login
+            logged_in = True  # Update the logged-in state
+            session['username'] = username  # Store the username in the session
+            return redirect('/')
+        else:
+            # Login failed, display an error message
+            return render_template('login.html', error='Invalid username or password')
+
+    return render_template('login.html')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global logged_in
+    if not logged_in:
+        return redirect('/login')  # Redirect to the login page if not logged in
+
     if request.method == 'POST':
         # Retrieve selected symptoms from the form submission
         selected_symptoms = request.form.getlist('symptoms')
